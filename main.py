11a98da28065153e1744fe69691d8ab687678e06
LegendaryFire <TristanBalon@Gmail.com>
2023-09-18 23:24:19 -0600

    Began development of run script.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import database
-import scraper
+import scraper as sc
 import config
 
 config = config.Config()
+search_queries = config.get_queries()
+scraper = sc.Scraper()
+
+database = database.Database()
+
+for query in search_queries:
+    results = scraper.search(query.build_url())
+    for ad in results:
+        exists = database.exists(ad)
+        if exists is False:
+            saved_ad = database.save_ad(ad)
+        else:
+            # Check for changes.
+            pass
