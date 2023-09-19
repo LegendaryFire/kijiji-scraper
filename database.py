64b110d55ec3c184a1a677ae1519bb0204a46429
LegendaryFire <TristanBalon@Gmail.com>
2023-09-18 23:07:51 -0600

    Implemented SQLAlchemy ORM and added docstrings.

--- database.py
+++ database.py
@@ -1,28 +1,92 @@
-import sqlite3
+from datetime import datetime, timezone
+from typing import Optional
+from sqlalchemy import select, create_engine, exists
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Ad(Base):
+    __tablename__ = "ad"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    is_business: Mapped[bool]
+    title: Mapped[str]
+    description: Mapped[str]
+    type: Mapped[str]
+    price: Mapped[str]
+    user_id: Mapped[str]
+    datetime_scraped: Mapped[datetime]
+    datetime_creation: Mapped[datetime]
+    datetime_start: Mapped[datetime]
+    datetime_end: Mapped[Optional[datetime]]
+    image: Mapped[Optional[str]]
 
 
 class Database:
     def __init__(self, filename="default.db"):
-        self.__connection = sqlite3.connect(filename)
-        self.__cursor = self.__connection.cursor()
-        try:
-            self.__cursor.execute("SELECT * FROM listing_data")
-        except sqlite3.OperationalError:
-            self.__init_database()
+        self.__engine = create_engine('sqlite:///default.db')
+        self.__engine.connect()
+        Base.metadata.create_all(self.__engine)
 
-    def add_listing(self):
-        pass
+    def exists(self, data=None, id=None) -> bool:
+        """
+        Checks if a specific ad exists either from JSON ad data, or a specified ad ID.
+        :param data: JSON ad data.
+        :param id: Ad ID.
+        :return: Returns true if the ad already exists in the database.
+        """
+        session = Session(self.__engine)
+        id = id if id is not None else data.get('@id')
+        return session.query(exists(Ad).where(Ad.id == id)).scalar()
 
-    def __init_database(self):
+    def save_ad(self, data) -> Ad:
         """
-        Initialize a new SQLite database. Creates the necessary tables for this application.
-        :return: Returns none.
+        Parses an ad from JSON ad data, and saves it to the database.
+        :param data: The JSON ad data.
+        :return: Returns the ad as an object.
         """
-        self.__cursor.execute("CREATE TABLE listing_data ("
-                              "id INTEGER NOT NULL PRIMARY KEY,"
-                              "title TEXT NOT NULL,"
-                              "description TEXT NOT NULL,"
-                              "price INTEGER NOT NULL"
-                              ")")
-        self.__cursor.close()
-        pass
+        id = data.get('@id')
+        is_business = id[0] == 'm'
+        title = data.get('ad:title')
+        description = data.get('ad:description')
+        type = data.get('ad:ad-type').get('ad:value').capitalize()
+        price = "Wanted" if type == "Wanted" \
+            else "Please Contact" if data.get('ad:price').get('types:amount') is None \
+            else data.get('ad:price').get('types:amount')
+        user_id = data.get('ad:user-id')
+        datetime_scraped = datetime.now(timezone.utc)
+        datetime_creation = datetime.strptime(data.get('ad:creation-date-time'), '%Y-%m-%dT%H:%M:%S.000Z').replace(tzinfo=timezone.utc)
+        datetime_start = datetime.strptime(data.get('ad:start-date-time'), '%Y-%m-%dT%H:%M:%S.000Z').replace(tzinfo=timezone.utc)
+        datetime_end = datetime.strptime(data.get('ad:end-date-time'), '%Y-%m-%dT%H:%M:%S.000Z').replace(tzinfo=timezone.utc) if data.get('ad:end-date-time') is not None else None
+        image = data.get('pic:pictures').get('pic:picture') if data.get('pic:pictures') is not None else None
+        if isinstance(image, list):
+            image = image[0].get('pic:link')
+        elif isinstance(image, dict):
+            image = image.get('pic:link')
+        if image is not None:
+            for i in range(len(image)):
+                if image[i].get('@rel') == 'extraLarge':
+                    image = image[i].get('@href')
+
+        session = Session(self.__engine)
+        ad = Ad(id=id,
+                is_business=is_business,
+                title=title,
+                description=description,
+                type=type,
+                price=price,
+                user_id=user_id,
+                datetime_scraped=datetime_scraped,
+                datetime_creation=datetime_creation,
+                datetime_start=datetime_start,
+                datetime_end=datetime_end,
+                image=image
+                )
+        session.add(ad)
+        session.commit()
+        return ad
+
+    def close(self):
+        self.__engine.dispose()
--- scraper.py
+++ scraper.py
@@ -1,49 +1,30 @@
+import logging
 import requests
 from constants import API_REQUEST_HEADERS
 import xmltodict
-from datetime import datetime,timezone
-
-
-class Ad:
-    def __init__(self, data):
-        self.__id = data.get('@id')
-        self.__is_business = self.__id[0] == 'm'
-        self.__title = data.get('ad:title')
-        self.__description = data.get('ad:description')
-        self.__type = data.get('ad:ad-type').get('ad:value').capitalize()
-        self.__price = "Wanted" if self.__type == "Wanted" \
-            else "Please Contact" if data.get('ad:price').get('types:amount') is None \
-            else data.get('ad:price').get('types:amount')
-        self.__user_id = data.get('ad:user-id')
-        self.__datetime_scraped = datetime.now(timezone.utc)
-        self.__datetime_creation = datetime.strptime(data.get('ad:creation-date-time'), '%Y-%m-%dT%H:%M:%S.000Z').replace(tzinfo=timezone.utc)
-        self.__datetime_start = datetime.strptime(data.get('ad:start-date-time'), '%Y-%m-%dT%H:%M:%S.000Z').replace(tzinfo=timezone.utc)
-        self.__datetime_end = datetime.strptime(data.get('ad:end-date-time'), '%Y-%m-%dT%H:%M:%S.000Z').replace(tzinfo=timezone.utc) if data.get('ad:end-date-time') is not None else None
-        self.__image = None
-        image = data.get('pic:pictures').get('pic:picture') if data.get('pic:pictures') is not None else None
-        if isinstance(image, list):
-            self.__image = image[0].get('pic:link')
-        elif isinstance(image, dict):
-            self.__image = image.get('pic:link')
-        if self.__image is not None:
-            for i in range(len(self.__image)):
-                if self.__image[i].get('@rel') == 'extraLarge':
-                    self.__image = self.__image[i].get('@href')
 
 
 class Scraper:
     def __init__(self):
         self.__session = requests.session()
 
-    def scrape(self, url):
+    def search(self, url):
+        """
+        Gets all ads for a given search query.
+        :param url: The URL of the search query.
+        :return: Returns ad data as a list of JSON objects.
+        """
         resp = self.__session.get(url, headers=API_REQUEST_HEADERS)
         if resp.status_code == 200:
-            raw_data = xmltodict.parse(resp.text)
-            if 'ad:ads' in raw_data.keys():
-                raw_data = raw_data['ad:ads'].get('ad:ad')
+            json_data = xmltodict.parse(resp.text)
+            if 'ad:ads' in json_data.keys():
+                json_data = json_data['ad:ads'].get('ad:ad')
                 ad_list = []
-                for raw_ad in raw_data:
-                    ad_list.append(Ad(raw_ad))
+                for json_ad in json_data:
+                    ad_list.append(json_ad)
                 return ad_list
+        else:
+            logging.warning(f"Unable to get search results. Status code {resp.status_code}.")
+            return None
 
 
