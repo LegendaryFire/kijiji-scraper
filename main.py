754b1af414ea6b27ac7d52654953e7e82a0d45f9
LegendaryFire <TristanBalon@Gmail.com>
2023-09-28 19:48:46 -0600

    Fix bug when unable to fetch search results.

--- main.py
+++ main.py
@@ -12,21 +12,22 @@ pushover = notifications.Pushover(config.get_pushover_token(), config.get_pushov
 while True:
     for query in config.get_queries():
         results = scraper.search(query.build_url())
-        for ad in results:
-            ad = scraper.parse_ad(ad)
-            exists = database.exists(ad)
-            if not exists:
-                # This is an ad we've never seen before.
-                database.save_ad(ad)
-                if query.send_notifications():
-                    if (ad.is_business and query.include_business()) or (not ad.is_business):
-                        pushover.send_notification(ad)
-            else:
-                # We've seen this ad before, let's look for any changes.
-                changes = database.compare(ad, True)
-                if len(changes) > 0:
+        if results:  # Make sure there are results to iterate through.
+            for ad in results:
+                ad = scraper.parse_ad(ad)
+                exists = database.exists(ad)
+                if not exists:
+                    # This is an ad we've never seen before.
+                    database.save_ad(ad)
                     if query.send_notifications():
                         if (ad.is_business and query.include_business()) or (not ad.is_business):
-                            pushover.send_notification(ad, changes)
+                            pushover.send_notification(ad)
+                else:
+                    # We've seen this ad before, let's look for any changes.
+                    changes = database.compare(ad, True)
+                    if len(changes) > 0:
+                        if query.send_notifications():
+                            if (ad.is_business and query.include_business()) or (not ad.is_business):
+                                pushover.send_notification(ad, changes)
 
     time.sleep(config.get_scan_interval())
